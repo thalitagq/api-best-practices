6137fe31e6ef7b94127509410a635b174ad1ad6f
thalita <thalitagq@outlook.com>
2022-11-27 09:51:07 -0300

    add project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const apicache = require("apicache");
+const app = express();
+const cache = apicache.middleware;
+const PORT = process.env.PORT || 3000;
+
+const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+
+app.use(express.json());
+app.use(cache("2 minutes"));
+app.use("/api/v1/workouts", v1WorkoutRouter);
+
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+});
